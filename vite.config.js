4665b0dc3483b0f1acc2563eda0fe061efbc8d5e
haotzar <haotzar-library@hotmail.com>
2026-05-03 10:07:10 +0300

    דוגמית

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: process.env.ELECTRON === 'true' ? './' : (process.env.VITE_BASE_PATH || '/'),
+  base: process.env.ELECTRON === 'true' ? './' : '/',
   plugins: [react()],
   server: {
     open: false,
